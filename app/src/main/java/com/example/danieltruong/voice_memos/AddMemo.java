6b8d01b920fb5cc45d0e5fc77b6661130bcdebc3
Daniel Truong <30559196+itkdaniel@users.noreply.github.com>
2018-05-28 19:53:36 -0700

    implemented gui

---
+++ app/src/main/java/com/example/danieltruong/voice_memos/AddMemo.java
@@ -0,0 +1,93 @@
+package com.example.danieltruong.voice_memos;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class AddMemo extends AppCompatActivity {
+
+    static final public String MYPREFS = "myprefs";
+    static final public String PREF_URL = "restore_url";
+    static final public String WEBPAGE_NOTHING = "about:blank";
+    static final public String MY_WEBPAGE = "https://users.soe.ucsc.edu/~dustinadams/CMPS121/assignment3/www/index.html";
+    static final public String LOG_TAG = "addMemo_webview";
+
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_memo);
+
+        myWebView = (WebView) findViewById(R.id.add_memo_webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        //Bind the js interface
+        myWebView.addJavascriptInterface(new JavaScriptInterface(this), "Android");
+        myWebView.loadUrl(MY_WEBPAGE);
+    }
+
+    public class JavaScriptInterface{
+        Context mContext;
+
+        JavaScriptInterface(Context c){
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public void record(){
+            Log.i(LOG_TAG, "I am in the js (record) call");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // TODO: METHOD CODE HERE
+                    Toast.makeText(AddMemo.this, "Recording", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        @JavascriptInterface
+        public void stop(){
+            Log.i(LOG_TAG, "I am in the js (stop) call");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // TODO: METHOD CODE HERE
+                    Toast.makeText(AddMemo.this, "Stopping", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        @JavascriptInterface
+        public void play(){
+            Log.i(LOG_TAG, "I am in the js (play) call");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // TODO: METHOD CODE HERE
+                    Toast.makeText(AddMemo.this, "Playing", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        @JavascriptInterface
+        public void stoprec(){
+            Log.i(LOG_TAG, "I am in the js (stoprec) call");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // TODO: METHOD CODE HERE
+                    Toast.makeText(AddMemo.this, "Stopping recording", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/danieltruong/voice_memos/MainActivity.java
+++ app/src/main/java/com/example/danieltruong/voice_memos/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.danieltruong.voice_memos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    public JSONObject jsonObject = null;
+    public JSONArray jsonArray = null;
+    private static final String TAG = "JSON_LIST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void onResume(){
+        super.onResume();
+        ListView list = findViewById(R.id.memo_list_view);
+        TextView text = findViewById(R.id.no_memos);
+        text.setVisibility(View.INVISIBLE);
+
+        jsonObject = null;
+
+        try{
+            File f = new File(getFilesDir(), "file.ser");
+            FileInputStream inputStream = new FileInputStream(f);
+            ObjectInputStream objectStream = new ObjectInputStream(inputStream);
+            String j = null;
+        }catch(IOException e){
+            list.setEnabled(false);
+            list.setVisibility(View.INVISIBLE);
+            text.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.add_memo:
+                startActivity(new Intent(MainActivity.this, AddMemo.class));
+                break;
+                default:
+                    super.onOptionsItemSelected(item);
+                    break;
+        }
+        return true;
     }
 }
