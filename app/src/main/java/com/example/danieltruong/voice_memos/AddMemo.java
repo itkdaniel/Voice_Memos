484b73a263b7a9c760d6c8868c193d5293c00221
Daniel Truong <30559196+itkdaniel@users.noreply.github.com>
2018-05-29 04:06:04 -0700

    added populate list view logic

--- app/src/main/java/com/example/danieltruong/voice_memos/AddMemo.java
+++ app/src/main/java/com/example/danieltruong/voice_memos/AddMemo.java
@@ -1,6 +1,13 @@
 package com.example.danieltruong.voice_memos;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +17,21 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class AddMemo extends AppCompatActivity {
 
     static final public String MYPREFS = "myprefs";
@@ -17,6 +39,12 @@ public class AddMemo extends AppCompatActivity {
     static final public String WEBPAGE_NOTHING = "about:blank";
     static final public String MY_WEBPAGE = "https://users.soe.ucsc.edu/~dustinadams/CMPS121/assignment3/www/index.html";
     static final public String LOG_TAG = "addMemo_webview";
+    public static final int RequestPermissionCode = 1;
+    String AudioSavePathInDevice = null;
+    MediaRecorder mediaRecorder;
+    MediaPlayer mediaPlayer;
+    JSONObject jsonObject = null;
+    JSONArray jsonArray = null;
 
     WebView myWebView;
 
@@ -33,6 +61,7 @@ public class AddMemo extends AppCompatActivity {
         //Bind the js interface
         myWebView.addJavascriptInterface(new JavaScriptInterface(this), "Android");
         myWebView.loadUrl(MY_WEBPAGE);
+
     }
 
     public class JavaScriptInterface{
@@ -50,6 +79,60 @@ public class AddMemo extends AppCompatActivity {
                 public void run() {
                     // TODO: METHOD CODE HERE
                     Toast.makeText(AddMemo.this, "Recording", Toast.LENGTH_LONG).show();
+
+                    // open file.ser
+                    try{
+                        File f = new File(getFilesDir(), "file.ser");
+                        FileInputStream inputStream = new FileInputStream(f);
+                        ObjectInputStream objectStream = new ObjectInputStream(inputStream);
+                        String j = null;
+
+                        try{
+                            j = (String) objectStream.readObject();
+//                            Log.d("ser_file_number: ", j);
+                            int num_memos = Integer.valueOf(j) + 1;
+                            j = String.valueOf(num_memos);
+                            Log.d("ser_file_number: ", j);
+
+                            try{
+                                FileOutputStream outputStream = new FileOutputStream(f);
+                                ObjectOutput objectOutput = new ObjectOutputStream(outputStream);
+                                objectOutput.writeObject(j);
+                                objectOutput.close();
+                                outputStream.close();
+                            }catch(IOException exception){
+
+                            }
+                        }catch(ClassNotFoundException c){
+                            c.printStackTrace();
+                        }
+                    }catch(IOException e){
+                        e.printStackTrace();
+                        Toast.makeText(AddMemo.this, "No Memos, creating first...", Toast.LENGTH_LONG).show();
+                        Log.d("add_status: ", "no memos, creating first");
+                        try{
+                            File f = new File(getFilesDir(), "file.ser");
+                            FileOutputStream outputStream = new FileOutputStream(f);
+                            ObjectOutput objectOutput = new ObjectOutputStream(outputStream);
+                            String j = "1";
+                            objectOutput.writeObject(j);
+                            objectOutput.close();
+                            outputStream.close();
+                        }catch(IOException io){
+                            io.printStackTrace();
+                        }
+
+                    }
+
+
+                    // read in the String to an int
+
+                    //add one to that int
+
+                    // convert back to a String (this is filename for your audio file)
+                    // audio file's extension is .3gp
+
+
                 }
             });
         }
@@ -98,9 +181,52 @@ public class AddMemo extends AppCompatActivity {
                 public void run() {
                     // TODO: METHOD CODE HERE
                     Toast.makeText(AddMemo.this, "Exiting activity", Toast.LENGTH_LONG).show();
-                    finish();
+                    // Must use startActivity() instead of finish() for listView to update in MainActivity onResume()
+                    startActivity(new Intent(AddMemo.this, MainActivity.class));
                 }
             });
+        }
+
+        public void MediaRecorderRead(){
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+            mediaRecorder.setOutputFile(AudioSavePathInDevice);
+        }
+
+        // TODO: REQUEST PERMISSIONS FROM USER
+        private void requestPermission(){
+            ActivityCompat.requestPermissions(AddMemo.this, new
+                    String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO}, RequestPermissionCode);
+        }
+
+        // TODO: CALLBACK METHOD
+        public void onRequestPermissionResult(int requestCode, String permissions[], int[] grantResults){
+            switch(requestCode){
+                case RequestPermissionCode:
+                    if (grantResults.length> 0) {
+                        boolean StoragePermission = grantResults[0] ==
+                                PackageManager.PERMISSION_GRANTED;
+                        boolean RecordPermission = grantResults[1] ==
+                                PackageManager.PERMISSION_GRANTED;
+
+                        if (StoragePermission && RecordPermission) {
+                            Toast.makeText(AddMemo.this, "Permission Granted",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(AddMemo.this,"Permission Denied",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    break;
+            }
+        }
+
+        public boolean checkPermission(){
+            int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+            int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+
+            return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
         }
     }
 }
--- app/src/main/java/com/example/danieltruong/voice_memos/MainActivity.java
+++ app/src/main/java/com/example/danieltruong/voice_memos/MainActivity.java
@@ -1,22 +1,27 @@
 package com.example.danieltruong.voice_memos;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,20 +34,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    @Override
     protected void onResume(){
         super.onResume();
         ListView list = findViewById(R.id.memo_list_view);
         TextView text = findViewById(R.id.no_memos);
         text.setVisibility(View.INVISIBLE);
 
-        jsonObject = null;
-
         try{
             File f = new File(getFilesDir(), "file.ser");
             FileInputStream inputStream = new FileInputStream(f);
             ObjectInputStream objectStream = new ObjectInputStream(inputStream);
             String j = null;
+
+            try{
+                j = (String) objectStream.readObject();
+                Log.d("ser_file_num: ", j);
+            }catch(ClassNotFoundException c){
+                c.printStackTrace();
+            }
+
+            int num_memos = Integer.valueOf(j);
+            String[] memo_titles = new String[num_memos];
+
+            Log.d("creating list_num: ", j);
+
+            for(int i = 0; i < num_memos; i++){
+                memo_titles[i] = "Audio Recording " + String.valueOf(i+1);
+            }
+
+            Log.d("list_status: ", "finished creating array");
+            ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, memo_titles);
+//            adapter.notifyDataSetChanged();
+            list.setAdapter(adapter);
+            Log.d("list_status: ", "finished creating and showing list");
+
+            for (int i = 0; i < num_memos; i++){
+                Log.d(" creating list item ", memo_titles[i]);
+            }
+
+            final Context context = this;
+
         }catch(IOException e){
             list.setEnabled(false);
             list.setVisibility(View.INVISIBLE);
